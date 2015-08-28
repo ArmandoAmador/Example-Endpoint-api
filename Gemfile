1f6cdddd387967aa00227979dae163312c31ee7f
Armando Amador <arm.amador@gmail.com>
2015-08-28 14:28:32 -0400

    Create listing endpoint
     - Add factory girl gem for factories
     - Add faker gem for fake data generation
     - Add pry gem for debugging
     - Create a listing factory
     - test listings index route and controller action

--- Gemfile
+++ Gemfile
@@ -15,4 +15,7 @@ end
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
+  gem 'factory_girl_rails'
+  gem 'faker'
+  gem 'pry'
 end
---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,7 @@
+class ListingsController < ApplicationController
+  def index
+    @listings = Listing.all
+
+    render json: @listings
+  end
+end
